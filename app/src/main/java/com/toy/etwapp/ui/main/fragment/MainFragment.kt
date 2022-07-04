854892ed524dcc8bc7bd77a19c56100664753095
leejuhyeon <esloven3@gmail.com>
2022-07-04 22:14:22 +0900

    Create Fragment, Activity >> Start Activity >> Contents Activity >> End Activity

--- app/src/main/java/com/toy/etwapp/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/com/toy/etwapp/ui/main/fragment/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.viewbinding.ViewBindings
 import com.toy.etwapp.R
 import com.toy.etwapp.ui.main.viewmodel.MainViewModel
 
@@ -16,18 +18,23 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var btn:Button;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
+
     }
+
 
 }
