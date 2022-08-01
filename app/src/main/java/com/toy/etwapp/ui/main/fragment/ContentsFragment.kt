0dde547c4022e04d55eb24b150558fad8861a311
leejuhyeon <esloven3@gmail.com>
2022-08-02 00:22:20 +0900

    Modify ContentsFragment, ModelView

--- app/src/main/java/com/toy/etwapp/ui/main/fragment/ContentsFragment.kt
+++ app/src/main/java/com/toy/etwapp/ui/main/fragment/ContentsFragment.kt
@@ -1,5 +1,6 @@
 package com.toy.etwapp.ui.main.fragment
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
@@ -7,16 +8,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.graphics.green
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.toy.etwapp.R
 import com.toy.etwapp.databinding.FragmentContentsBinding
+import com.toy.etwapp.ui.main.activity.ContentsActivity
 import com.toy.etwapp.ui.main.viewmodel.ContentsViewModel
 
-class ContentsFragment : Fragment() {
+class ContentsFragment : Fragment(), View.OnClickListener {
 
     companion object {
         fun newInstance() = ContentsFragment()
@@ -33,8 +37,10 @@ class ContentsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentContentsBinding.inflate(inflater, container, false)
-
         val v = inflater.inflate(R.layout.fragment_contents, container, false)
+        var img1: ImageView = vv.findViewById(R.id.contents_img1)
+        var img2: ImageView = vv.findViewById(R.id.contents_img2)
+
         vv = v
 
         return binding.root
@@ -45,8 +51,7 @@ class ContentsFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ContentsViewModel::class.java)
         // TODO: Use the ViewModel
 
-        var img1: ImageView = vv.findViewById(R.id.contents_img1)
-        var img2: ImageView = vv.findViewById(R.id.contents_img2)
+
 
         /*
         Glide.with(vv)
@@ -57,6 +62,22 @@ class ContentsFragment : Fragment() {
             .load("http://192.168.0.11:8080" + response.body()!!.data.get(1).imgPath.toString())
             .into(img2)
         */
+    }
+
+    override fun onClick(v: View?) {
+        when (vv.id) {
+            R.id.contents_img1 -> {
+                Toast.makeText(context,"Select !", Toast.LENGTH_LONG).show()
+
+            }
+            R.id.contents_img2 -> {
+
+            }
+        }
+    }
+
+    fun changeView(){
+        
     }
 
     override fun onDestroy() {
